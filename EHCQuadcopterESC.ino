16d3d87fbccb23e27c38e058c2f45e0b52e5c41e
Droid5x <mrkblnc1248@gmail.com>
2014-11-03 18:17:06 -0500

    Code Simplification
    Simplifies pin setting in code

--- EHCQuadcopterESC.ino
+++ EHCQuadcopterESC.ino
@@ -2,18 +2,19 @@
 // Authors: Mark Blanco and Alex Buchholz
 // 10/20/2014
 
-
+int potentiometer = 7;
 int pin_2 = 2;
 int pin_3 = 3;
 int pin_4 = 4;
 int pin_5 = 5;
 int pin_6 = 6;
 int pin_7 = 7;
-int delay_value = 3;
+float delay_value = 5;
 
 //odd numbers are NChannels substituting PChannels
 
 void setup(){
+  Serial.begin(9600);
   for (int i = 2; i < 8; i++){
     pinMode(i,OUTPUT);
     digitalWrite(i,LOW);
@@ -21,47 +22,30 @@ void setup(){
 }
 
 void loop(){
-  digitalWrite(2,HIGH);
-  digitalWrite(5,HIGH);
-  digitalWrite(3,LOW);
-  digitalWrite(4,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(7,LOW);
-  delay(delay_value);
-  digitalWrite(2,HIGH);
+  delay_value = ( float(analogRead(potentiometer)) * 0.00488759 );
+  Serial.println(delay_value);
   digitalWrite(7,HIGH);
-  digitalWrite(3,LOW);
-  digitalWrite(4,LOW);
-  digitalWrite(6,LOW);
   digitalWrite(5,LOW);
   delay(delay_value);
-  digitalWrite(4,HIGH);
-  digitalWrite(7,HIGH);
-  digitalWrite(3,LOW);
-  digitalWrite(2,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(5,LOW);
-  delay(delay_value);
-  digitalWrite(3,HIGH);
+  delay_value = ( float(analogRead(potentiometer)) * 0.00488759 );
   digitalWrite(4,HIGH);
   digitalWrite(2,LOW);
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(7,LOW);
   delay(delay_value);
+  delay_value = ( float(analogRead(potentiometer)) * 0.00488759 );
   digitalWrite(3,HIGH);
-  digitalWrite(6,HIGH);
-  digitalWrite(2,LOW);
-  digitalWrite(4,LOW);
-  digitalWrite(5,LOW);
   digitalWrite(7,LOW);
   delay(delay_value);
-  digitalWrite(5,HIGH);
+  delay_value = ( float(analogRead(potentiometer)) * 0.00488759 );
   digitalWrite(6,HIGH);
-  digitalWrite(2,LOW);
-  digitalWrite(3,LOW);
   digitalWrite(4,LOW);
-  digitalWrite(7,LOW);
+  delay(delay_value);
+  delay_value = ( float(analogRead(potentiometer)) * 0.00488759 );
+  digitalWrite(5,HIGH);
+  digitalWrite(3,LOW);
+  delay(delay_value);
+  delay_value = ( float(analogRead(potentiometer)) * 0.00488759 );
+  digitalWrite(2,HIGH);
+  digitalWrite(6,LOW);
   delay(delay_value);
 }
 
